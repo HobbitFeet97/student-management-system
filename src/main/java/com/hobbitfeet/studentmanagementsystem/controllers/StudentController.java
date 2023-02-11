c1f27a8b0e529581e18eaad1efe63642e35aa022
AJ <andrewt565@gmail.com>
2023-02-11 17:42:09 +0100

    New end-point for student creation and functionality.

--- src/main/java/com/hobbitfeet/studentmanagementsystem/controllers/StudentController.java
+++ src/main/java/com/hobbitfeet/studentmanagementsystem/controllers/StudentController.java
@@ -1,10 +1,13 @@
 package com.hobbitfeet.studentmanagementsystem.controllers;
 
+import com.hobbitfeet.studentmanagementsystem.entities.Student;
 import com.hobbitfeet.studentmanagementsystem.services.api.StudentApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,6 +19,14 @@ public class StudentController {
     public ResponseEntity getAllStudents(){
         try {
             return ResponseEntity.ok(studentApi.getAllStudents());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+    @PostMapping
+    public ResponseEntity createNewStudent(@RequestBody Student newStudent) {
+        try {
+            return ResponseEntity.ok(studentApi.createStudent(newStudent));
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
--- src/main/java/com/hobbitfeet/studentmanagementsystem/services/api/StudentApi.java
+++ src/main/java/com/hobbitfeet/studentmanagementsystem/services/api/StudentApi.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface StudentApi {
     List<Student> getAllStudents();
+    Student createStudent(Student student);
 }
